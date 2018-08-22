a418e8f2e3921f56ba7e525efb9fb30d4a31f709
Ayato <yokoyamahyounin@ayatonomacbook-puro.local>
2018-08-22 14:12:38 +0900

    Data persisted using UserDefaults method.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //MARK - Tableview Datasource Methods
@@ -49,12 +53,13 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once the user clicks the Add Item button on our UIAlert
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         // ↓のクロージャはaddTextFieldがされた時、つまり+ボタンが押された時に呼び出される
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new Item" //optionで調べればalertTextFieldの型がUITextFieldだとわかる　だからvar textField = UITextField()
-            textField = alertTextField
+            textField = alertTextField //他のスコープで使えるようにグローバル変数に格納
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
